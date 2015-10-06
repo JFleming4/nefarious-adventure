a9aac9f50b6c7edabd4700f8f7f9c5fe5392f5e7
Derek Stride <djgstride@gmail.com>
2015-10-06 14:39:29 -0400

    Device now conforms to message api

--- device.c
+++ device.c
@@ -10,9 +10,7 @@
 int main(int argc, char *argv[]) {
     int msgid;
     int pid = getpid();
-    int val;
-    message_t sensor_data;
-    message_t server_response, my_message;
+    message_t sensor_data, server_response;
 
     if (argc < 4) {
         fprintf(stderr, "Not enough args, you gave %d, 4 were required!\n", argc);
@@ -34,39 +32,27 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if (msgsnd(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT) == -1) {
-        fprintf(stderr, "msgsnd failed: %d\n", errno);
-        exit(EXIT_FAILURE);
-    }
-
-    if (msgrcv(msgid, (void *)&server_response, sizeof(server_response.packet),
-               pid, 0) == -1) {
-        fprintf(stderr, "msgrcv failed with error: %d\n", errno);
-        exit(EXIT_FAILURE);
-    }
+    send_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT);
     
+    receive_msg(msgid, (void *)&server_response, sizeof(server_response.packet), (long int) pid, 0);
+
     if(!server_response.packet.value) {
         fprintf(stderr, "the acknowladge failed for proccess: %d\n", pid);
         exit(EXIT_FAILURE);
     }
-
+    
     printf("Sent:\n");
     print_message(&sensor_data);
     printf("Recieved:\n");
     print_message(&server_response);
     
-    my_message.msg_type = MESSAGE_KEY;
-    my_message.packet.pid = pid;
+    sensor_data.msg_type = MESSAGE_KEY;
+    sensor_data.packet.pid = pid;
     
     while(1) {
-        val = rand() % 36;
-        
-        my_message.packet.value = val;
-        
-        if(msgsnd(msgid, (void *)&my_message, sizeof(my_message.packet), IPC_NOWAIT) == -1) {
-            fprintf(stderr, "msgsnd failed: %d\n", errno);
-            exit(EXIT_FAILURE);
-        }
+        sensor_data.packet.value = rand() % 36;
+        send_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT);
+        printf("Current Value: %d\n", sensor_data.packet.value);
         sleep(2);
     }
     
