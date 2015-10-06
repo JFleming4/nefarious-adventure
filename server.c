2ecbb4cde0b3ef69b83f4a8cabc639d993931e98
Derek Stride <djgstride@gmail.com>
2015-10-06 15:40:37 -0400

    Working on controller functionality

--- server.c
+++ server.c
@@ -3,17 +3,48 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/msg.h>
 
 #include "message.h"
 
 #define RECEIVE_TYPE -2L
+#define MAX_DEVICES 8
+
+typedef struct {
+    pid_t pid;
+    int type;
+    int threshold;
+    char name[BUFFER_SIZE];
+} device_t;
+
+// Register a device with the controller by adding it to the devices array.
+void register_device(device_t *device, message_t *msg) {
+    device->pid = msg->packet.pid;
+    device->type = msg->packet.type;
+    device->threshold = msg->packet.threshold;
+    strcpy(device->name, msg->packet.name);
+}
+
+// Returns the index of the device that matches the sender of the message.
+// Returns -1 if it's not found.
+int find_device(message_t *msg, device_t devices[MAX_DEVICES]) {
+    for (int i = 0; i < MAX_DEVICES; i++) {
+        if (devices[i].pid == msg->packet.pid) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 
 int main(void) {
-    int running = 1;
     int msgid;
     message_t data_received, data_transmition;
+    device_t current_device, devices[MAX_DEVICES];
+    int next_device = 0;
+    int current_index;
 
     // Setup the message queue
     msgid = msgget((key_t) 1234, 0666 | IPC_CREAT);
@@ -25,6 +56,17 @@ int main(void) {
 
     while(1) {
         receive_msg(msgid, (void *)&data_received, sizeof(data_received.packet), RECEIVE_TYPE, 0);
+        
+
+        if (data_received.msg_type == REGISTER_KEY) {
+            register_device(&devices[next_device++], &data_received);
+        } else if (data_received.msg_type == MESSAGE_KEY) {
+            current_index = find_device(&data_received, &devices, MAX_DEVICES);
+            if (find_device(&devices, &data_received) == -1) {
+                continue;
+            }
+            current = devices[current_index];
+        }
         
     	data_transmition.msg_type = (long int) data_received.packet.pid;
         data_transmition.packet.pid = getpid();
