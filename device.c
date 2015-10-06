86ffa508b78b4f7e9c90991f04e93f2cd9fe2241
Justin <justinfleming95@gmail.com>
2015-10-06 15:38:45 -0400

    added the actuator code

--- device.c
+++ device.c
@@ -7,36 +7,56 @@
 
 #include "message.h"
 
+// When it is a sensor device this function will be called
 void sensor(int pid, int msgid) {
-    message_t sensor_data;
+    message_t sensor_data;  // struct to hold message for sending
     
-    sensor_data.msg_type = MESSAGE_KEY;
-    sensor_data.packet.pid = pid;
+    // This data won't change and is set outside the loop
+    sensor_data.msg_type = MESSAGE_KEY;     // set message type
+    sensor_data.packet.pid = pid;           // set pid
+    sensor_data.packet.type = SENSOR;       // set it to type sensor
     
     while(1) {
-        sensor_data.packet.value = rand() % 36;
-        send_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT);
-        printf("Current Value: %d\n", sensor_data.packet.value);
-        sleep(2);
+        sensor_data.packet.value = rand() % 36; // Generate a random value 0 - 35
+        send_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT); //send to server
+        printf("Current Value: %d\n", sensor_data.packet.value); // print to console
+        sleep(2);   //wait 2 seconds
+    }
+}
+
+// When it is an actuator this function will be called
+void actuator(int pid, int msgid, char *name) {
+    message_t sensor_data;  // struct to hold server response
+    
+    while(1) {
+        receive_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), (long int) pid,
+        0); // wait for message from server
+        printf("%s was turned on", name);   // print a message to console
+        sensor_data.packet.pid = pid;   // set pid
+        sensor_data.packet.value = 1;   // set value to true
+        sensor_data.packet.type = ACTUATOR; // set type to actuator
+        send_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT);  // send back response to server
     }
 }
 
 int main(int argc, char *argv[]) {
-    int msgid;
-    int pid = getpid();
-    message_t sensor_data, server_response;
+    int msgid;  // msgid is used to locate the right message queue
+    int pid = getpid(); // get the process id
+    int type;   // sensor or actuator
+    message_t sensor_data, server_response; // sensor or actuator
 
     if (argc < 4) {
         fprintf(stderr, "Not enough args, you gave %d, 4 were required!\n", argc);
         exit(EXIT_FAILURE);
-    } else {
+    } else {    // copy the command line arguments to the sensor_data struct
         strcpy(sensor_data.packet.name, argv[1]);
         sscanf(argv[2], "%d", &sensor_data.packet.type);
         sscanf(argv[3], "%d", &sensor_data.packet.threshold);
     }
-
-    sensor_data.packet.pid = pid;
-    sensor_data.msg_type = REGISTER_KEY;
+    
+    type = sensor_data.packet.type; // save the type in case of corruption
+    sensor_data.packet.pid = pid;   // set the pid
+    sensor_data.msg_type = REGISTER_KEY;    // set this as a register message
 
     // Make sure queue is there
     msgid = msgget((key_t)1234, 0666);
@@ -45,11 +65,14 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "msgget failed with error: %d\n", errno);
         exit(EXIT_FAILURE);
     }
-
+    
+    // send the register message
     send_msg(msgid, (void *)&sensor_data, sizeof(sensor_data.packet), IPC_NOWAIT);
     
+    // wait for an acknowladgement
     receive_msg(msgid, (void *)&server_response, sizeof(server_response.packet), (long int) pid, 0);
-
+    
+    // if the acknowladgement is false
     if(!server_response.packet.value) {
         fprintf(stderr, "the acknowladge failed for proccess: %d\n", pid);
         exit(EXIT_FAILURE);
@@ -59,7 +82,13 @@ int main(int argc, char *argv[]) {
     print_message(&sensor_data);
     printf("Recieved:\n");
     print_message(&server_response);
-    sensor(pid, msgid);
+    
+    // check to see if this is an actuator or sensor
+    if(type) {
+        actuator(pid, msgid, sensor_data.packet.name);
+    } else {
+        sensor(pid, msgid);
+    }
     
     
     exit(EXIT_SUCCESS);
